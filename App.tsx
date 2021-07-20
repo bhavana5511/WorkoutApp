fc84c649872fad23475ea2c468a8af681701df49
bhavana5511 <47493530+bhavana5511@users.noreply.github.com>
2021-07-20 12:55:12 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text,View} from "react-native";
+
+const App: React.FC = () => {
+  return(
+    <view>
+      <text>
+        hi
+      </text>
+    </view>
+
+  );
+};
+
+export default App;
